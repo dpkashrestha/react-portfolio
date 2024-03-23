1eee98ee3a6060c851425df477abdfdc97f2ac04
dpkashrestha <dpkashrestha7@gmail.com>
2024-03-23 02:08:29 -0500

    Using static port for server

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 app.use(bodyParser.json());
 
