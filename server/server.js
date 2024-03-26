db43036b5b8b83d688b7d79ec7d8b61828120a11
dpkashrestha <dpkashrestha7@gmail.com>
2024-03-26 00:05:06 -0500

    Update server and client to use process port

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const path = require("path");
 require("dotenv").config();
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
