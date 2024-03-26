84ae1e0d48e2108161e08212c59b3f0fb518e99d
dpkashrestha <dpkashrestha7@gmail.com>
2024-03-25 23:47:37 -0500

    Add missing dependency

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
